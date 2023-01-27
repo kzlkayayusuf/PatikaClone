d06056ac86356ee141fcd0f1d4ebe0396c9c3d02
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-01-27 21:38:15 +0300

    added helper class

---
+++ patikaClone/src/com/patikadev/helper/Helper.java
@@ -0,0 +1,29 @@
+package com.patikadev.helper;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Helper {
+    public static void setLayout() {
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                try {
+                    UIManager.setLookAndFeel(info.getClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                         UnsupportedLookAndFeelException e) {
+                    throw new RuntimeException(e);
+                }
+                //System.out.println("Nimbus teması bulundu!");
+                break;
+            }
+        }
+    }
+
+    public static int screenCenter(String axis, Dimension size) {
+        return switch (axis) {
+            case "x" -> (Toolkit.getDefaultToolkit().getScreenSize().width - size.width) / 2;
+            case "y" -> (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2;
+            default -> 0;
+        };
+    }
+}
